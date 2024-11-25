cdddf6034641e6ba2ca24f0e94326a08d8e4db5b
Carrington-dev <crn96m@gmail.com>
2024-11-25 22:39:22 +0200

    Added complete but init atantla project

---
+++ includes/details.php
@@ -0,0 +1,12 @@
+<?php 
+  $company = "Atlanta Projects";
+  $page = "About";
+  $phone = "+27 67 869 5716";
+//   $tel = "+27 61 431 9286";
+  $adminmail = "crn96m@gmail.com";
+  $email = "info@atlantlaprojects.co.za";
+  $replytomail = $email;
+  $address = "395 Francis Baard Street<br>Pretoria, Gauteng 0001";
+  $copyright = "<p>Copyright &copy; 2014-" . date("Y") . " ". $company . '. All rights reserved.';
+
+  ?>
